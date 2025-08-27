e4d9815c2190f00cee68c1afb1cab6e3a9d1f222
KimberlyCanteberos <kimberlycanteberos@gmail.com>
2025-08-27 09:46:33 +0800

    Firebase Config

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBcEDVIGey1u8NrgWCO0doi60veV5pRcvE",
+  authDomain: "cjo-goals.firebaseapp.com",
+  projectId: "cjo-goals",
+  storageBucket: "cjo-goals.firebasestorage.app",
+  messagingSenderId: "455406483071",
+  appId: "1:455406483071:web:238924a4b09e3167cdb14a"
+};
+
+const app = initializeApp(firebaseConfig);
